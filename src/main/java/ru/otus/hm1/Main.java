5f5d709c236f2219ac143b5d9873e60f03891949
DarSky24 <DarSky24@users.noreply.github.com>
2018-07-02 21:06:27 +0300

    homework2

--- src/main/java/ru/otus/hm1/Main.java
+++ src/main/java/ru/otus/hm1/Main.java
@@ -1,13 +1,16 @@
 package ru.otus.hm1;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import ru.otus.hm1.domain.Student;
 import ru.otus.hm1.service.InterviewService;
 
+@Configuration
+@ComponentScan
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("/spring-context.xml");
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Main.class);
         InterviewService service = ctx.getBean(InterviewService.class);
         service.conductInterview(new Student());
     }
--- src/main/java/ru/otus/hm1/service/InterviewServiceImpl.java
+++ src/main/java/ru/otus/hm1/service/InterviewServiceImpl.java
@@ -1,19 +1,27 @@
 package ru.otus.hm1.service;
 
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
 import ru.otus.hm1.dao.InterviewDao;
 import ru.otus.hm1.domain.Interview;
 import ru.otus.hm1.domain.Student;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
+import static java.lang.System.lineSeparator;
+
+@Service
 public class InterviewServiceImpl implements InterviewService {
 
     private InterviewDao dao;
+    private MessageSource ms;
 
-    public InterviewServiceImpl(InterviewDao dao) {
+    public InterviewServiceImpl(InterviewDao dao, MessageSource ms) {
         this.dao = dao;
+        this.ms = ms;
     }
 
     private static int checkAnswers(List<Integer> studentsAnswers, List<Integer> rightAnswers) {
@@ -24,6 +32,23 @@ public class InterviewServiceImpl implements InterviewService {
             }
         }
         return result;
+    }
+
+    private void printQuestion(Interview question) {
+        System.out.println(ms.getMessage("interview.number", null, Locale.getDefault()) + question.getNumber()
+                + ":" + lineSeparator() + question.getQuestion() + lineSeparator()
+                + ms.getMessage("interview.answers", null, Locale.getDefault()) + lineSeparator() + "1. "
+                + question.getAnswers().get(0) + lineSeparator()
+                + "2. " + question.getAnswers().get(1) + lineSeparator()
+                + "3. " + question.getAnswers().get(2) + lineSeparator()
+                + ms.getMessage("interview.enter", null, Locale.getDefault()));
+    }
+
+    private void printResult(Student student) {
+        System.out.println(ms.getMessage("student.name", null, Locale.getDefault()) + student.getName() + " "
+                + student.getSurname()
+                + ", " + ms.getMessage("student.result1", null, Locale.getDefault()) + student.getResult() + " "
+                + ms.getMessage("student.result2", null, Locale.getDefault()));
     }
 
     @Override
@@ -39,22 +64,22 @@ public class InterviewServiceImpl implements InterviewService {
             List<Integer> studentsAnswers = new ArrayList<>();
             int answer;
             Scanner in = new Scanner(System.in);
-            System.out.println("Введите имя:");
+            System.out.println(ms.getMessage("enter.name", null, Locale.getDefault()));
             String input = in.nextLine();
             student.setName(input);
-            System.out.println("Введите фамилию:");
+            System.out.println(ms.getMessage("enter.surname", null, Locale.getDefault()));
             input = in.nextLine();
             student.setSurname(input);
             for (Interview question : questions) {
-                System.out.println(question);
+                printQuestion(question);
                 answer = in.nextInt();
                 studentsAnswers.add(answer);
                 rightAnswers.add(question.getRightNumber());
             }
             student.setResult(checkAnswers(studentsAnswers, rightAnswers));
-            System.out.println(student);
+            printResult(student);
         } catch (Exception e) {
-            System.out.println("Проблема с получением списка вопросов!");
+            System.out.println(ms.getMessage("exp.message", null, Locale.getDefault()));
         }
     }
 
